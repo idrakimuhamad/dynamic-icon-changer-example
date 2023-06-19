6826b316129d9aa1c96c60f26c43704ea36269e1
Idraki Muhamad <idrakimuhamad@gmail.com>
2023-06-19 15:38:31 +0800

    init

--- App.js
+++ App.js
@@ -1,7 +1,24 @@
+import 'expo-dev-client';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { changeIcon, getIcon } from 'react-native-change-icon';
+import { useEffect } from 'react';
 
 export default function App() {
+  useEffect(() => {
+    async function checkAndApplyicon() {
+      const currentIcons = await getIcon()
+
+      if (currentIcons === 'festive') {
+        changeIcon('normal')
+      } else {
+        changeIcon('festive')
+      }
+    }
+
+    checkAndApplyicon()
+  }, [])
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
