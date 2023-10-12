6a10c066373d7f317b73ef08b973c08638f5d7e4
Idraki Muhamad <idrakimuhamad@gmail.com>
2023-10-12 12:32:05 +0800

    chore: update the resources

--- App.js
+++ App.js
@@ -1,28 +1,59 @@
 import 'expo-dev-client';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 import { changeIcon, getIcon } from 'react-native-change-icon';
-import { useEffect } from 'react';
 
 export default function App() {
-  useEffect(() => {
-    async function checkAndApplyicon() {
-      const currentIcons = await getIcon()
+  const [icon, setIcon] = useState('Default')
 
-      if (currentIcons === 'festive') {
-        changeIcon('normal')
-      } else {
-        changeIcon('festive')
+  const updateIcon = async () => {
+    if (icon === 'Default') {
+      setIcon('Festive')
+
+      try {
+        const response = await changeIcon('Festive')
+
+        console.log('response', response);
+      } catch (error) {
+        console.log('error', error);
+      }
+    } else {
+      setIcon('Default')
+
+      try {
+        const response = await changeIcon('Default')
+        
+        console.log('response', response);
+      } catch (error) {
+        console.log('error', error);
       }
     }
+  }
 
-    checkAndApplyicon()
+  useEffect(() => {
+    async function checkIcon() {
+      const currentIcon = await getIcon()
+
+      setIcon(currentIcon)
+
+      console.log('currentIcon', currentIcon);
+    }
+
+    checkIcon()
   }, [])
 
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text>Current icon: {icon}</Text>
+      <Pressable onPress={updateIcon} style={{
+        padding: 16,
+        backgroundColor: '#5CB5F9'
+      }}>
+        <Text>Change to {icon === 'Default' ? 'festive' : 'default'} icon</Text>
+        </Pressable>
     </View>
   );
 }
@@ -33,5 +64,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    gap: 16
   },
 });
